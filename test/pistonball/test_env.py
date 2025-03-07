aa1aa6defa1728f63bd8d1e1c7be2aea983350e3
billhu0228@icloud.com <billhu0228@icloud.com>
2025-03-07 18:12:44 +0800

    20250307-3

--- test/pistonball/test_env.py
+++ test/pistonball/test_env.py
@@ -1,37 +1,7 @@
 from bridgezoo import pistonball_v6
-
-
-def run(env_func, num_games, **kwargs):
-    env = env_func.env(**kwargs)
-    env.reset()
-    rewards = {agent: 0 for agent in env.possible_agents}
-    for k in range(num_games):
-        env.reset()
-        for agent in env.agent_iter():
-            obs, reward, termination, truncation, info = env.last()
-            for a in env.agents:
-                rewards[a] += env.rewards[a]
-            if termination or truncation:
-                break
-            else:
-                act = env.unwrapped.action_space(agent).sample()
-            env.step(act)
-        print(rewards)
-    env.close()
-
+from pettingzoo.test import api_test
 
 if __name__ == "__main__":
-    env_fn = cablebridge_v1
-    env_kwargs = dict(
-        beam_w=10.0,
-        beam_h=1.0,
-        num_cables_per_side=6,
-        anchor_height=20,
-        max_cycles=100,
-        render_mode="",
-        DEF_SCALE=10,
-        FPS=10,
-    )
-
-    env_kwargs['render_mode'] = 'text'
-    run(env_fn, num_games=10, **env_kwargs)
+    env = pistonball_v6.env(render_mode="human")
+    env.reset()
+    api_test(env, num_cycles=1000, verbose_progress=False)
